b2ae7a438e31ded3d8ef29ed452299d289991519
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-10-04 23:24:43 +0400

    Example for from book

---
+++ src/chapter2/Example2.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int a;
+        a = 100;
+        System.out.println("փոփոխական a=" + a);
+        a = a * 2;
+        System.out.print("փոփոխական * 2 հավասար է ");
+
+        System.out.println(a);
+    }
+}
