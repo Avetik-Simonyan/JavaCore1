70f3ff0f1f81a928f8b015e450b3b082a9f119ba
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-10-09 01:16:00 +0400

    Homework FigurePainter3

---
+++ src/Homework/FigurePainter3.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class FigurePainter3 {
+    public static void main(String[] args) {
+        int i, j, k = 4;
+        for (i = 0; i < k; i++) {
+            for (j = (k - i); j >= 1; j--) {
+                System.out.print("* ");
+            }
+            for (j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
