58e4af44b5ba89a0017bab7742e1df71f0cddea4
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-10-09 00:54:33 +0400

    Homework FigurePainter2

--- src/chapter2/For.java
+++ src/chapter2/For.java
@@ -5,8 +5,8 @@ public class For {
 
         int x;
 
-        for (x = 0; x<10; x = x+1)
+        for (x = 0; x < 10; x = x + 1)
 
-        System.out.println("x:" + x );
+            System.out.println("x:" + x);
     }
 }
