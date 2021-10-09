b1f07e14b992095587767d03a4e96bdbfe2417aa
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-10-10 01:13:43 +0400

    Homework FigurePainter5

---
+++ src/Homework/FigurePainter5.java
@@ -0,0 +1,26 @@
+package Homework;
+
+public class FigurePainter5 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 4; i++){
+            for(int j = i; j<=4; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k<=i-1; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        int k = 4;
+        for (int i = k-1; i >= 0; i--) {
+            for (int j = k; j >= i+1; j--) {
+                System.out.print(" ");
+            }
+            for (int l = 1; l <= i; l++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+
+    }
+}
