f5556878632d7e0d002a49f3e155a3803856d1f0
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-10-13 01:01:59 +0400

    Homework FigurePainter Առաջին 4 պատկերները ստացվել է, վերջին պատկերի մեջ վերջին 2 տողը սխալ է գալիս, պակաս է for մտնում, չկարողացա գտնել ինչից է խնդիրը

--- src/Homework/FigurePainter5.java
+++ src/Homework/FigurePainter5.java
@@ -2,18 +2,18 @@ package Homework;
 
 public class FigurePainter5 {
     public static void main(String[] args) {
-        for(int i = 0; i <= 4; i++){
-            for(int j = i; j<=4; j++){
+        for (int i = 0; i <= 4; i++) {
+            for (int j = i; j <= 4; j++) {
                 System.out.print(" ");
             }
-            for(int k = 0; k<=i-1; k++){
+            for (int k = 0; k <= i - 1; k++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         int k = 4;
-        for (int i = k-1; i >= 0; i--) {
-            for (int j = k; j >= i+1; j--) {
+        for (int i = k - 1; i >= 0; i--) {
+            for (int j = k; j >= i + 1; j--) {
                 System.out.print(" ");
             }
             for (int l = 1; l <= i; l++) {
