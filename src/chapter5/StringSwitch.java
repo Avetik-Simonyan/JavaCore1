e7f4bf50f4de00a33f92617ff2dfe27c7f027857
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-11-05 02:33:00 +0400

    chapter 5 examples

---
+++ src/chapter5/StringSwitch.java
@@ -0,0 +1,21 @@
+package chapter5;
+
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "hing";
+        switch (str) {
+            case "mek":
+                System.out.println("mek");
+                break;
+            case "erku":
+                System.out.println("erku");
+                break;
+            case "ereq":
+                System.out.println("ereq");
+                break;
+            default:
+                System.out.println("chhamapatasxanec");
+                break;
+        }
+    }
+}
