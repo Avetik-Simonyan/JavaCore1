c1991473b3e4ba7e4e48166f55a166a0f94a5ee9
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-11-07 23:00:09 +0400

    student example

---
+++ src/classwork/StudentTest.java
@@ -0,0 +1,9 @@
+package classwork;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student("Poxos", "Poxosyan", 23, "098059779", "Java");
+        System.out.println(student);
+
+    }
+}
