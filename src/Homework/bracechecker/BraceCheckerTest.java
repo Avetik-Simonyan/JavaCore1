72644fc5fdec8d89988c269595b3cb722f95b975
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-11-09 01:47:38 +0400

    Brace Checker

---
+++ src/Homework/bracechecker/BraceCheckerTest.java
@@ -0,0 +1,10 @@
+package Homework.bracechecker;
+
+public class BraceCheckerTest {
+    public static void main(String[] args) {
+        String text = "Hello (from) [Java}";
+        BraceChecker braceChecker = new BraceChecker();
+        braceChecker.check();
+        System.out.println("Error: opened [ but closed } at 20");
+    }
+}
