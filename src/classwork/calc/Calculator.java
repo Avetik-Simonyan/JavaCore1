ba771586fc6487ecbf731d956b4513e28766b591
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-11-10 02:24:13 +0400

    Calculator

---
+++ src/classwork/calc/Calculator.java
@@ -0,0 +1,19 @@
+package classwork.calc;
+
+public class Calculator {
+    int plus(int a, int b) {
+        return a + b;
+    }
+
+    int minus(int a, int b) {
+        return a - b;
+    }
+
+    double divide(int a, int b) {
+        return a / b;
+    }
+
+    long multiply(int a, int b) {
+        return a * b;
+    }
+}
