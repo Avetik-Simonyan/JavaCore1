4d1988996dcad847a5fcb421de0f410cbfec8275
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-11-17 16:29:06 +0400

    Dynamic array

--- src/Homework/dynamicArray/DynamicArray.java
+++ src/Homework/dynamicArray/DynamicArray.java
@@ -7,9 +7,51 @@ public class DynamicArray {
     public void add(int value) {
         if (array.length == size) {
             extand();
-
         }
         array[size++] = value;
+    }
+
+    public void add(int[] numbers) {
+        for (int number : numbers) {
+            add(number);
+        }
+    }
+
+    public void add(int value, int index) {
+        if (index < 0 || index > size) {
+            System.err.println("invalid index" + index);
+        } else {
+            if (array.length == size) {
+                extand();
+            }
+            for (int i = size; i >= index; i--) {
+                array[i + 1] = array[i];
+            }
+            array[index] = value;
+            size++;
+        }
+    }
+
+    public void set(int value, int index) {
+        if (index < 0 || index > size) {
+            System.err.println("invalid index" + index);
+        } else {
+            array[index] = value;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isExists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+
+        }
+        return false;
     }
 
     private void extand() {
@@ -31,7 +73,18 @@ public class DynamicArray {
     public void print() {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
 
+    void delete(int index) {
+        if (index < 0 || index > size) {
+            System.err.println("invalid index");
+        } else {
+            for (int i = index + 1; i < size; i++) {
+                array[i - 1] = array[i];
+            }
+            size--;
         }
     }
 }
--- src/Homework/dynamicArray/DynamicArrayTest.java
+++ src/Homework/dynamicArray/DynamicArrayTest.java
@@ -3,6 +3,23 @@ package Homework.dynamicArray;
 public class DynamicArrayTest {
     public static void main(String[] args) {
         DynamicArray dy = new DynamicArray();
+        System.out.println(dy.isEmpty());
+        dy.add(5);
+        System.out.println(dy.isEmpty());
+
+        int[] numbers = {33, 44, 55};
+
+        dy.add(numbers);
+
+        dy.print();
+
+        dy.add(66, 2);
+
+        dy.print();
+
+        System.out.println(dy.isExists(66));
+    }
+}
 //        dy.add(33);
 //        dy.add(43);
 //        dy.add(53);
@@ -16,11 +33,19 @@ public class DynamicArrayTest {
 //        dy.add(83);
 //        dy.add(83);
 //        dy.add(83);
-        for(int i = 0; i < 1000; i++){
-            dy.add(i + 1);
-        }
 
-        System.out.println(dy.getByIndex(1003));
-        dy.print();
-    }
-}
+
+//        for(int i = 0; i < 10; i++){
+//            dy.add(i + 1);
+//        }
+//
+//        dy.print();
+//
+//        dy.delete(9);
+
+//        dy.print();
+//
+////        System.out.println(dy.getByIndex(1003));
+////        dy.print();
+//    }
+//}
