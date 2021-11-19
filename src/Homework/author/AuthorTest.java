0d9b60a91ce0e341438e9712ff47057831838a3d
Avetik Simonyan <avetiksimonyan91@gmail.com>
2021-11-20 00:06:03 +0400

    Book, BookStorage ավելացված է

--- src/Homework/author/AuthorTest.java
+++ src/Homework/author/AuthorTest.java
@@ -8,11 +8,16 @@ public class AuthorTest {
     static Scanner scanner = new Scanner(System.in);
     static AuthorStorage authorStorage = new AuthorStorage();
 
+    static BookStorage bookStorage = new BookStorage();
+
     private static final String EXIT = "0";
     private static final String ADD_AUTHOR = "1";
     private static final String SEARCH_AUTHOR = "2";
     private static final String SEARCH_AUTHOR_BY_AGE = "3";
     private static final String PRINT_AUTHOR = "4";
+    private static final String ADD_BOOK = "5";
+    private static final String PRINT_BOOKS = "6";
+    private static final String SEARCH_BOOKS_BY_TITLE = "7";
 
     public static void main(String[] args) {
         authorStorage.add(new Author("poxos", "poxosyan", 25, "poxos@mail.ru", "male"));
@@ -37,10 +42,41 @@ public class AuthorTest {
                 case PRINT_AUTHOR:
                     authorStorage.print();
                     break;
+                case ADD_BOOK:
+                    addBook();
+                    break;
+                case PRINT_BOOKS:
+                    bookStorage.print();
+                    break;
+                case SEARCH_BOOKS_BY_TITLE:
+                    SearchBooksByTitle();
+                    break;
+
                 default:
                     System.out.println("Invalid command!!!");
             }
         }
+    }
+
+    private static void addBook() {
+        System.out.println("please input book's name");
+        String title = scanner.nextLine();
+        System.out.println("please input book's description");
+        String description = scanner.nextLine();
+        System.out.println("please input book's price");
+        double price = Double.parseDouble(scanner.nextLine());
+        System.out.println("please input count of books");
+        int count = Integer.parseInt(scanner.nextLine());
+
+        Book book = new Book(title, description, price, count);
+        bookStorage.add(book);
+        System.out.println("Thank you, book was added");
+    }
+
+    private static void SearchBooksByTitle() {
+        System.out.println("please input book's info");
+        String keyword = scanner.nextLine();
+        bookStorage.searchBookByTitle(keyword);
     }
 
     private static void searchByAge() {
@@ -55,8 +91,11 @@ public class AuthorTest {
         System.out.println("please input " + EXIT + " for EXIT");
         System.out.println("please input " + ADD_AUTHOR + " for add author");
         System.out.println("please input " + SEARCH_AUTHOR + " for search author by name");
-        System.out.println("please input " + SEARCH_AUTHOR_BY_AGE + " for search author by name");
+        System.out.println("please input " + SEARCH_AUTHOR_BY_AGE + " for search author by age");
         System.out.println("please input " + PRINT_AUTHOR + " for print authors");
+        System.out.println("please input " + ADD_BOOK + " for add books");
+        System.out.println("please input " + PRINT_BOOKS + " for print books");
+        System.out.println("please input " + SEARCH_BOOKS_BY_TITLE + " for search book by title");
     }
 
     private static void searchByName() {
---
+++ src/Homework/author/BookStorage.java
@@ -0,0 +1,33 @@
+package Homework.author;
+
+public class BookStorage {
+    private Book[] books = new Book[16];
+    private int size;
+
+    public void add(Book book) {
+        if (books.length == size) {
+            extand();
+        }
+        books[size++] = book;
+    }
+
+    private void extand() {
+        Book[] tmp = new Book[books.length + 10];
+        System.arraycopy(books, 0, tmp, 0, books.length);
+        books = tmp;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(books[i]);
+        }
+    }
+
+    public void searchBookByTitle(String keyword) {
+        for (int i = 0; i < size; i++) {
+            if (books[i].getTitle().contains(keyword)) {
+                System.out.println(books[i]);
+            }
+        }
+    }
+}
